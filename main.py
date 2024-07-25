8381d4596eb8bb0301398db94566ce1a3be8aeeb
Sreehari <sreeharickotoor@gmail.com>
2024-07-25 14:04:16 +0530

    added a method to view transactions btw given range

--- main.py
+++ main.py
@@ -31,6 +31,35 @@ class CSV:
             writer = csv.DictWriter(csv_file,fieldnames=cls.COLUMNS)
             writer.writerow(new_entry)
 
+    @classmethod
+    def get_transactions(cls,start_date,end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df['date'] = pd.to_datetime(df['date'],format=cls.FORMAT)
+        start_date = datetime.strptime(start_date,cls.FORMAT)
+        end_date = datetime.strptime(end_date,cls.FORMAT)
+
+        mask = (df["date"]>=start_date) & (df["date"]<=end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print("No transaction is present in between the entered date")
+        else:
+            print("Transactions:")
+            print(
+                filtered_df.to_string(
+                    index=False,formatters={"date":lambda x: x.strftime(CSV.FORMAT)}
+                    )
+                )
+            total_income = filtered_df[filtered_df["category"]=="Income"]["amount"].sum()
+            total_expense = filtered_df[filtered_df["category"]=="Expense"]["amount"].sum()
+
+            print(f"Total income is : {total_income:.2f}")
+            print(f"Total expense is : {total_expense:.2f}")
+
+        return filtered_df
+
+
+
 def add():
     CSV.initialize()
     date = get_date("Enter the date in the format(dd-mm-yyyy)",allow_default=True)
@@ -39,4 +68,26 @@ def add():
     description = get_description()
     CSV.add_entry(date,amount,category,description)
 
-add()
+def main():
+    while True:
+        print("\n1.Add new Transactions:")
+        print("\n2.View Transactions and get summery:")
+        print("\n3.Exit")
+
+        choice = input()
+        if choice=="1":
+            add()
+        elif choice=="2":
+            start_date = get_date("Enter the start date : ")
+            end_date = get_date("Enter the end date : ")
+            CSV.get_transactions(start_date,end_date)
+        elif choice=="3":
+            print("Exiting........")
+            break
+        else:
+            print("Invalid choice")
+
+if __name__=="__main__":
+    main()
+        
+
